5cdb31c7205fcfb3385cd20209db3b52474bd489
mikan68 <a68world@gmail.com>
2017-12-05 14:44:18 +0900

    ログアウト処理 インターフェースの調整

---
+++ www/js/Option.js
@@ -0,0 +1,15 @@
+// This is a JavaScript file
+
+//ログアウト処理
+function logout() {
+    
+    var res = confirm("ログアウトしてもいいですか？");
+    if( res == true ) {
+        // OKなら移動
+        window.localStorage.clear();
+        location.href = "./index.html";   
+    }
+    else {
+    }
+  
+}

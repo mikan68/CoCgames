8f778843c119cbd287a9c87994ebf36cc3009bb0
mikan68 <a68world@gmail.com>
2018-01-21 14:50:47 +0900

    charcter 追加

--- www/js/db/play_character.js
+++ www/js/db/play_character.js
@@ -46,5 +46,6 @@ function charaInsert(){
     db.transaction(function (tx){
         tx.executeSql('insert into play_character(pc_id,pc_name,rank,str,dex,pc_int,con,app,pow,siz,edu,profile,characteristic01,characteristic02,characteristic03,pl) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', [1,'睦亮太郎',1,15,13,15,9,9,12,12,15,'胃が痛い医大生',23,15,22,2])
         tx.executeSql('insert into play_character(pc_id,pc_name,rank,str,dex,pc_int,con,app,pow,siz,edu,profile,characteristic01,characteristic02,characteristic03,pl) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', [2,'狩野陽子',1,13,10,14,12,14,14,11,16,'図書館司書さん',50,0,0,2])    
+        tx.executeSql('insert into play_character(pc_id,pc_name,rank,str,dex,pc_int,con,app,pow,siz,edu,profile,characteristic01,characteristic02,characteristic03,pl) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', [3,'アンバー=サットン',1,10,11,15,13,15,10,9,14,'イギリス在住のそれなりに名の知れた考古学者。自分の思う通りにいかないと気が済まない傲慢な性格。資産家の娘であり、学者である両親の影響か珍しいものが大好きで手元に置きたがる。',72,0,0,2])
     })
 }

7ae2d1fa0322ad5a1d6a48f69bccec93f21eaf8e
mikan68 <a68world@gmail.com>
2018-03-03 17:14:18 +0900

    ログアウト前にテーブルを更新する

--- www/js/Option.js
+++ www/js/Option.js
@@ -3,14 +3,24 @@ function logout() {
     
     var res = confirm("ログアウトしてもいいですか？");
     if( res == true ) {
-        // OK
-        //ここでuserテーブル更新させる************
-        
+        //ログアウト前に更新*********************
+        userUpdate();
         //****************************************
         window.localStorage.clear();
         location.href = "./index.html";   
     }
-    else {
-    }
   
+}
+
+//usersテーブル更新
+function userUpdate(){
+    var loginUser = JSON.parse(localStorage.getItem('loginUser')); //login user情報
+    var id = loginUser["id"];
+    var mainChara = JSON.parse(localStorage.getItem('mainChara'));
+    var sp = JSON.parse(localStorage.getItem('sp'));
+    
+    db.transaction(function (tx) {
+        tx.executeSql('update users set summon_point = ? where id = ?', [sp, id])
+        tx.executeSql('update users set mainChara = ? where id = ?', [mainChara, id])
+    })
 }

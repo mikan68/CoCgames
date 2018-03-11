5bc231b4dbcf761361775414b1e55ecbe9ef968b
mikan68 <a68world@gmail.com>
2018-03-11 14:49:28 +0900

    ログアウト後更新

--- www/js/Option.js
+++ www/js/Option.js
@@ -7,7 +7,7 @@ function logout() {
         userUpdate();
         pc_info2Update();
         //****************************************
-        //window.localStorage.clear();
+        window.localStorage.clear();
         location.href = "./index.html";   
     }
   
@@ -26,26 +26,33 @@ function userUpdate(){
     })
 }
 
-//pc_info2更新
+//pc_info2更新***キャラ追加ごとに記述が必要
 function pc_info2Update(){
     var possessionCharaList = JSON.parse(localStorage.getItem('possessionCharaList'));
+    console.log(typeof(possessionCharaList[0]["bondsPoint"]));
     
-    for(var i=0; i<possessionCharaList.length; i++ ){
-        var id = possessionCharaList[i]["id"];
-        var boundsPoint = possessionCharaList[i]["boundsPoint"];
-        var evolutionaryStage = possessionCharaList[i]["evolutionaryStage"];
-        var numberOfSummons = possessionCharaList[i]["numberOfSummons"];
-        var possessionFlag = possessionCharaList[i]["possessionFlag"];
-        var summonDate = possessionCharaList[i]["summonDate"];
-        
-        db.transaction(function (tx) {
-            tx.executeSql('update pc_info2 set bp = ? where id = ?', [boundsPoint, id])
-            tx.executeSql('update pc_info2 set nos = ? where id = ?', [numberOfSummons, id])
-            tx.executeSql('update pc_info2 set es = ? where id = ?', [evolutionaryStage, id])
-            tx.executeSql('update pc_info2 set pf = ? where id = ?', [possessionFlag, id])
-            tx.executeSql('update pc_info2 set sd = ? where id = ?', [summonDate, id])
-        })
-    }
+    db.transaction(function (tx) {
+        tx.executeSql('update pc_info2 set bp = ?, nos = ?, es = ?,sd = ? where id = ?;', [possessionCharaList[0]["bondsPoint"], possessionCharaList[0]["numberOfSummons"], possessionCharaList[0]["evolutionaryStage"],possessionCharaList[0]["summonDate"], possessionCharaList[0]["id"]])
+        tx.executeSql('update pc_info2 set bp = ?, nos = ?, es = ?,sd = ? where id = ?;', [possessionCharaList[1]["bondsPoint"], possessionCharaList[1]["numberOfSummons"], possessionCharaList[1]["evolutionaryStage"],possessionCharaList[1]["summonDate"], possessionCharaList[1]["id"]])
+        tx.executeSql('update pc_info2 set bp = ?, nos = ?, es = ?,sd = ? where id = ?;', [possessionCharaList[2]["bondsPoint"], possessionCharaList[2]["numberOfSummons"], possessionCharaList[2]["evolutionaryStage"],possessionCharaList[2]["summonDate"], possessionCharaList[2]["id"]])
+    })
+    
+    // for(var i=0; i<possessionCharaList.length; i++ ){
+    //     var id = possessionCharaList[i]["id"];
+    //     var boundsPoint = possessionCharaList[i]["bondsPoint"];
+    //     var evolutionaryStage = possessionCharaList[i]["evolutionaryStage"];
+    //     var numberOfSummons = possessionCharaList[i]["numberOfSummons"];
+    //     var possessionFlag = possessionCharaList[i]["possessionFlag"];
+    //     var summonDate = possessionCharaList[i]["summonDate"];
+    //     
+    //     db.transaction(function (tx) {
+    //         tx.executeSql('update pc_info2 set bp = ? where id = ?;', [bondsPoint, id])
+    //         tx.executeSql('update pc_info2 set nos = ? where id = ?', [numberOfSummons, id])
+    //         tx.executeSql('update pc_info2 set es = ? where id = ?', [evolutionaryStage, id])
+    //         tx.executeSql('update pc_info2 set pf = ? where id = ?', [possessionFlag, id])
+    //         tx.executeSql('update pc_info2 set sd = ? where id = ?', [summonDate, id])
+    //     })
+    // }
 }
 
 
